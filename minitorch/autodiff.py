4c2ee02cb219583cb8271b88d580b2e5cf278e1f
Ryan Hamor <HamorRyanB@JohnDeere.com>
2025-10-14 13:55:31 -0500

    add autodiff

---
+++ minitorch/autodiff.py
@@ -0,0 +1,181 @@
+from dataclasses import dataclass
+from typing import Any, Iterable, List, Tuple
+
+from typing_extensions import Protocol
+
+
+def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) -> Any:
+    r"""
+    Computes an approximation to the derivative of `f` with respect to one arg.
+
+    See :doc:`derivative` or https://en.wikipedia.org/wiki/Finite_difference for more details.
+
+    Args:
+        f : arbitrary function from n-scalar args to one value
+        *vals : n-float values $x_0 \ldots x_{n-1}$
+        arg : the number $i$ of the arg to compute the derivative
+        epsilon : a small constant
+
+    Returns:
+        An approximation of $f'_i(x_0, \ldots, x_{n-1})$
+    """
+    vals1 = [v for v in vals]
+    vals2 = [v for v in vals]
+    vals1[arg] = vals1[arg] + epsilon
+    vals2[arg] = vals2[arg] - epsilon
+    delta = f(*vals1) - f(*vals2)
+    return delta / (2 * epsilon)
+
+
+variable_count = 1
+
+
+class Variable(Protocol):
+    def accumulate_derivative(self, x: Any) -> None:
+        """
+        Accumulates the derivative (gradient) for this Variable.
+
+        Args:
+            x (Any): The gradient value to be accumulated.
+        """
+        pass
+
+    @property
+    def unique_id(self) -> int:
+        """
+        Returns:
+            int: The unique identifier of this Variable.
+        """
+        pass
+
+    def is_leaf(self) -> bool:
+        """
+        Returns whether this Variable is a leaf node in the computation graph.
+
+        Returns:
+            bool: True if this Variable is a leaf node, False otherwise.
+        """
+        pass
+
+    def is_constant(self) -> bool:
+        """
+        Returns whether this Variable represents a constant value.
+
+        Returns:
+            bool: True if this Variable is constant, False otherwise.
+        """
+        pass
+
+    @property
+    def parents(self) -> Iterable["Variable"]:
+        """
+        Returns the parent Variables of this Variable in the computation graph.
+
+        Returns:
+            Iterable[Variable]: The parent Variables of this Variable.
+        """
+        pass
+
+    def chain_rule(self, d_output: Any) -> Iterable[Tuple["Variable", Any]]:
+        """
+        Implements the chain rule to compute the gradient contributions of this Variable.
+
+        Args:
+            d_output (Any): The gradient of the output with respect to the Variable.
+
+        Returns:
+            Iterable[Tuple[Variable, Any]]: An iterable of tuples, where each tuple
+                contains a parent Variable and the corresponding gradient contribution.
+        """
+        pass
+
+
+def topological_sort(variable: Variable) -> Iterable[Variable]:
+    """
+    Computes the topological order of the computation graph.
+
+    Args:
+        variable: The right-most variable
+
+    Returns:
+        Non-constant Variables in topological order starting from the right.
+    """
+    # BEGIN ASSIGN1_1
+    ordered_vars = []
+    visited = set()
+
+    def recursive_search(var):
+        if not var.unique_id in visited:
+            if not var.is_constant():
+                visited.add(var.unique_id)
+                for parent in var.parents:
+                    recursive_search(parent)
+                ordered_vars.append(var)
+
+    recursive_search(variable)
+    
+    # Need the order reversed so root is first
+    return list(reversed(ordered_vars))
+    # END ASSIGN1_1
+
+
+def backpropagate(variable: Variable, deriv: Any) -> None:
+    """
+    Runs backpropagation on the computation graph in order to
+    compute derivatives for the leave nodes.
+
+    Args:
+        variable: The right-most variable
+        deriv  : Its derivative that we want to propagate backward to the leaves.
+
+    No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
+    """
+    # BEGIN ASSIGN1_1
+    # Create list of variable and upstream derivates for tracking
+    derivatives: {int, Any} = {variable.unique_id: deriv}
+
+    # Need to get sorted list
+    ordered_vars = topological_sort(variable)
+
+    for var in ordered_vars:
+        if var.unique_id not in derivatives:
+            continue
+
+        output_deriv = derivatives[var.unique_id]
+
+        # Accumulate derivatives if this is a leave.
+        if var.is_leaf():
+            var.accumulate_derivative(output_deriv)
+            continue
+
+        for parent, input_deriv in var.chain_rule(output_deriv):
+            if parent.is_constant():
+                continue
+
+            if parent.unique_id in derivatives:
+                # Accumulate if multiple children contribute to same parent.
+                derivatives[parent.unique_id] = derivatives[parent.unique_id] + input_deriv
+            else:
+                derivatives[parent.unique_id] = input_deriv
+
+    # END ASSIGN1_1
+    
+
+@dataclass
+class Context:
+    """
+    Context class is used by `Function` to store information during the forward pass.
+    """
+
+    no_grad: bool = False
+    saved_values: Tuple[Any, ...] = ()
+
+    def save_for_backward(self, *values: Any) -> None:
+        "Store the given `values` if they need to be used during backpropagation."
+        if self.no_grad:
+            return
+        self.saved_values = values
+
+    @property
+    def saved_tensors(self) -> Tuple[Any, ...]:
+        return self.saved_values
